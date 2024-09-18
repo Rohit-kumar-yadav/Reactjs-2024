9bc253a9faec1e489a82eecd941aaf441d07987b
Rohit kumar yadav <76595116+Rohit-kumar-yadav@users.noreply.github.com>
2024-09-18 23:05:34 +0530

    temp

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import GlobalState from "./context/index.jsx";
-
+import Main from "../react-router-and-custom-hooks/src/main.jsx"
 createRoot(document.getElementById("root")).render(
   <GlobalState>
-    <App />
+    <Main/>
   </GlobalState>
 );
